ee47375fa0a3ed3c78679980fba6f35d1d195487
Vishav Gupta <82940442+VishavGupta01@users.noreply.github.com>
2026-04-23 23:52:13 +0530

    Learning: Create a Hello World SpringBoot WebApp - Create SpringBoot App from IDE (IntelliJ Idea Ultimate Edition) - Create SpringBoot App from start.spring.io - Adding Dependencies in the SpringBoot App

---
+++ Spring/dependancy-injection-demo/src/main/java/com/Vishav/dependancyinjectiondemo/DependancyInjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Vishav.dependancyinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependancyInjectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependancyInjectionDemoApplication.class, args);
+    }
+
+}
---
+++ Spring/dependancy-injection-demo/src/test/java/com/Vishav/dependancyinjectiondemo/DependancyInjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vishav.dependancyinjectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependancyInjectionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
